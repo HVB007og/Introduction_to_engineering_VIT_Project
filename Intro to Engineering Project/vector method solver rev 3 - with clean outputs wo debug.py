cc1fb0106810254eef4f8f46db9d491e53872dba
Harsh Bopalia <harsh.bopalia@gmail.com>
2023-10-28 17:59:11 +0530

    coding - working v2
    removed infinite loop conditions when maze not solvable

--- Intro to Engineering Project/vector method solver rev 3 - with clean outputs wo debug.py
+++ Intro to Engineering Project/vector method solver rev 3 - with clean outputs wo debug.py
@@ -1,14 +1,11 @@
 import math
-import time
 
 
 def check_in_history(coord):
     for movement in movehistory[::-1]:
         if coord == movement[0]:
-            print('in History')
             return True
     else:
-        print('not in History')
         return False
 
 
@@ -20,7 +17,6 @@ def check_Junction(coord):
 
 
 def block_wall(coord, Direction):
-    print("called block_wall(coord, Direction):")
     if Direction == 0:
         maze[coord[0] - 1][coord[1]] = 1
     elif Direction == 1:
@@ -34,26 +30,22 @@ def block_wall(coord, Direction):
 
 def jump_to_last_junc():
     global currentpos, lastpos, movehistory
-    print("called jump_to_last_junc():")
     for movement in movehistory[::-1]:
-        print('  ', movement, check_Junction(movement[0]))
         if check_Junction(movement[0]):
             movehistory = list(movehistory[0: movehistory.index(movement)])
-            print("new move history:")
             printlist(movehistory)
             block_wall(movement[0], movement[2])
             currentpos, lastpos = list(movement[0]), list(movement[1])
-            print("jumped To", currentpos, lastpos)
             break
     else:
         # condition when no feasible path to final
-        print("no path")
         return -1
 
 
 def printlist(list):  # For debugging
-    for line in list:
-        print(*line)
+    # for line in list:
+    #     print(*line)
+        pass
 
 
 def getcord(d, x, p=None):
@@ -73,12 +65,9 @@ def getcord(d, x, p=None):
 
 
 def length(x, y):
-    # print("lengthcalled:",[currentpos[0]+x,currentpos[1]+y],'length:', end=' ')
-
     if x == 0 and y == 0:
         return -1
     val = math.sqrt((currentpos[0] + x - finalpos[0]) ** 2 + (currentpos[1] + y - finalpos[1]) ** 2)
-    # print(val)
     return val
 
 
@@ -108,7 +97,6 @@ def check_walls(posi=0):
 
 def movepos(d):
     global currentpos, lastpos
-    print("movepos called:", d)
 
     movehistory.append([currentpos, lastpos, d])
     logichistory.append([currentpos, lastpos, d])
@@ -121,12 +109,10 @@ def movepos(d):
         currentpos = list([currentpos[0], currentpos[1] - 2])
     elif d == 3:
         currentpos = list([currentpos[0], currentpos[1] + 2])
-    print("after Moving:", currentpos, lastpos)
 
 
 def check_optimal_move():
     global lastpos
-    print("called check optimal move", end='')
     lengths = []
     if dir[0] == 1:
         lengths.append([0, length(-2, 0)])
@@ -141,21 +127,19 @@ def check_optimal_move():
     second_optimal = [0, 0]
     least = 100000000000
     secondleast = 100000000000
-    # print('com lengths:', lengths)
-    for i in lengths:
-        if i[1] < least:
-            current_optimal = list(i)
-            least = i[1]
-            lengths.pop(lengths.index(i))
+    for item in lengths:
+        if item[1] < least:
+            current_optimal = list(item)
+            least = item[1]
+            lengths.pop(lengths.index(item))
 
-    # print('com lengths:', lengths)
     if len(lengths) == 1:
         second_optimal = list(lengths[0])
     else:
-        for i in lengths:
-            if i[1] < secondleast:
-                second_optimal = list(i)
-                secondleast = i[1]
+        for item in lengths:
+            if item[1] < secondleast:
+                second_optimal = list(item)
+                secondleast = item[1]
 
     if getcord(second_optimal[0], 2) == lastpos:
         lengths.pop(lengths.index(second_optimal))
@@ -163,13 +147,11 @@ def check_optimal_move():
             second_optimal = lengths[0]
         else:
             second_optimal[0] = -1
-    print('', current_optimal[0], second_optimal[0])
     return current_optimal[0], second_optimal[0]
 
 
 def change_direction(direction):
     global current_direction
-    # print(current_direction, direction, ':', end='')
     if current_direction == 0 and direction == 0 or current_direction == 1 and direction == 3 or \
             current_direction == 2 and direction == 1 or current_direction == 3 and direction == 2:
         return 'Do nothing'
@@ -221,11 +203,8 @@ logichistory = []
 
 solved = False
 run, i = True, int()
-while run and i < 50:
-    print("****************************************************************"
-          "\n", i, "data:", currentpos, lastpos, "\n\nHistory:")
+while run and i < 200:
     printlist(movehistory)
-    print()
 
     if currentpos == finalpos:
         print("Maze solved optimally!")
@@ -239,7 +218,6 @@ while run and i < 50:
         pass
 
     noplaces, dir = check_walls()
-    print('Check walls output', noplaces, dir)
 
     if noplaces == 0:
         break
@@ -266,8 +244,6 @@ while run and i < 50:
         else:
             movepos(dir1)
         i += 1
-    # time.sleep(1)
-
 
 
 if solved:
